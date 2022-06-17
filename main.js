3094f465333190ae503a7fd30e9a25e93b2f419e
Kipleting <kipletingedwin4@gmail.com>
2022-06-17 22:13:21 +0300

    commit tree ccc055c4b0b875f799c1c9c61ce625ed76a8934e

--- main.js
+++ main.js
@@ -1,15 +1,44 @@
-// Defining text characters for the empty and full hearts for you to use later.
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.querySelector("#modal");
+modal.classList.add("hidden");
 
 
+const click = (event) =>{
+  const heart = event.target
+  heart.innerText = EMPTY_HEART
+  mimicServerCall()
+    .then( res => {
+      if(heart.innerText === EMPTY_HEART){
+        heart.innerText = FULL_HEART
+        heart.classList.add("activated-heart")
+      }
+      else{
+        heart.innerText = EMPTY_HEART
+        heart.classList.remove("activated-heart")
+      }
+      
+      
+    })
+    .catch(err => {
+      modal.classList.remove("hidden");
+      modal.querySelector("#modal-message").textContent = err;
+      setTimeout( () => {
+        modal.classList.add("hidden")
+      }, 3000)
+    }) 
+   
+}
 
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
+
+const hearts = document.querySelectorAll(".like-glyph");
+hearts.forEach( heart => {
+  heart.addEventListener("click", click)
+})
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
@@ -23,3 +52,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+console.log(mimicServerCall);
